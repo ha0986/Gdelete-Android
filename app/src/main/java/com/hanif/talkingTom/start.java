9b04a43fdfa08dc520fb78b497b1aaf14ef43ddb
ha0986 <ha0986@outlook.com>
2023-01-12 20:48:28 +0600

    master

--- app/src/main/java/com/hanif/talkingTom/start.java
+++ app/src/main/java/com/hanif/talkingTom/start.java
@@ -2,7 +2,10 @@ package com.hanif.talkingTom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class start extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button web = findViewById(R.id.web);
+        Button more = findViewById(R.id.more);
+        ImageButton youTube = findViewById(R.id.youTube);
+
+
+
+        web.setOnClickListener(v -> {
+            Intent myIntent = new Intent(start.this, webview.class);
+            startActivity(myIntent);
+        });
+
+        more.setOnClickListener(v -> {
+            Intent myIntent = new Intent(start.this, more.class);
+            startActivity(myIntent);
+        });
+
+        youTube.setOnClickListener(v -> {
+
+        });
     }
 }
