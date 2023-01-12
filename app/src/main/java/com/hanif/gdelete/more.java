2d3723a2822a38bdc2e58b9176720186dd1b842a
ha0986 <ha0986@outlook.com>
2023-01-12 11:59:24 +0600

    master

---
+++ app/src/main/java/com/hanif/gdelete/more.java
@@ -0,0 +1,14 @@
+package com.hanif.gdelete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class more extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
